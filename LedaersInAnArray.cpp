5234ed5e816dbaa7b375ae328a6a50ccc8543d32
Sumit <sumitsingh.connect@gmail.com>
2025-09-05 22:29:22 +0530

    Solved leaders in an array

---
+++ LedaersInAnArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+ vector<int> arr={16,17,4,3,5,2},leaders;
+ int size=arr.size()-1,temp=INT16_MIN;
+for(int i=size;i>=0;i--){
+   
+
+    if(arr[i]>temp){
+         temp=arr[i];
+        leaders.push_back(arr[i]);
+    }
+
+   
+}
+   reverse(leaders.begin(),leaders.end());
+
+   for(int val:leaders){
+    cout<<val << " ";
+   }
+
+
+
+    return 0;
+}
