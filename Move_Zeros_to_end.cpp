92a9b36ad1861c8b2df963952199b49a95059e3b
Sumit <sumitsingh.connect@gmail.com>
2025-10-09 22:40:11 +0530

    some minor changes in the code

--- Move_Zeros_to_end.cpp
+++ Move_Zeros_to_end.cpp
@@ -5,15 +5,9 @@ using namespace std;
 
 int main(){
     vector<int>arr={1, 2, 0, 4, 3, 0, 5, 0};    
-    int j=-1,n=arr.size();
+    int j=0,n=arr.size();
         
-        for(int i=0;i<n;i++){
-            
-            if(arr[i]==0){
-                j=i;
-                break;
-            }
-        }
+    
         
         for(int i=j+1;i<n;i++){
             
